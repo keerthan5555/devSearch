f1b85cc7ed6390eb64145ef99c3ce8514a4f8880
trial-start <143869677+trial-start@users.noreply.github.com>
2023-09-22 21:40:20 +0530

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -183,7 +183,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sample2003test@gmail.com'
-EMAIL_HOST_PASSWORD = 'qorxctawebhnoymj'
+EMAIL_HOST_PASSWORD = ''
 
 
 # Static files (CSS, JavaScript, Images)
